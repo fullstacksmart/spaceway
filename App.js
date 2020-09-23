12601064bdd75b7aa78219a9f7b5e92a143cda53
pintoedo <eduardoandrespo@gmail.com>
2020-09-23 16:19:31 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import { Video } from 'expo-av';
+
+const videoSource = require('./assets/mars.mp4');
+const logoSource = require('./assets/logo.png');
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Image source={logoSource}  style={styles.logo}></Image>
+    {/* <Text style={styles.text}>SPACEWAY</Text>
+    <Text style={styles.slogan}>explore beyond.</Text> */}
+     <Video style={styles.video}
+      source={videoSource}
+      rate={1.2}
+      isMuted={true}
+      resizeMode="cover"
+      shouldPlay
+      isLooping
+      style={{ width: Dimensions.get('window').width, height:  Dimensions.get('window').height - 200 }}
+    />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +31,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  video: {
+    position: 'absolute',
+  },
+  logo: {
+    resizeMode: 'contain',
+    height: 90,
+    marginTop: 50,
+  }
 });
